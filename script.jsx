bf93f701d1ce6fdf4b34dc7dad65af15cad969e4
sharkyu3 <lowhuiyu@hotmail.com>
2019-09-01 20:08:47 +0800

    deleted items added to deleted list

--- script.jsx
+++ script.jsx
@@ -1,3 +1,17 @@
+class DeletedItem extends React.Component {
+
+    render(){
+        const wordsList = this.props.deleted.map((word, index) => {
+        return (<div className="box" value={index}>
+                <p>{word[0]}</p>
+                <p>{moment(word[1]).format("DD MMM YYYY, h:mm:ss a")}</p>
+            </div>);
+      });
+        return(<div> Deleted Items<div>{wordsList}</div></div>)
+    }
+}
+
+
 class ToDoItem extends React.Component {
 
     render(){
@@ -10,7 +24,6 @@ class ToDoItem extends React.Component {
       });
         return(<div>{wordsList}</div>)
     }
-
 }
 
 class List extends React.Component {
@@ -21,18 +34,20 @@ class List extends React.Component {
       word:"",
       list : [],
       class: "",
-      warning: ""
+      warning: "",
+      deleted: []
     }
     this.delete = this.delete.bind(this);
   }
 
   delete(event){
+    this.state.deleted.push(this.state.list[event.target.value]);
     this.state.list.splice(event.target.value,1);
     let newState = {
+        deleted: this.state.deleted,
         list: this.state.list
     };
     this.setState(newState);
-    console.log("end of delete function", this.state.list);
   }
 
   addItem(){
@@ -57,7 +72,12 @@ class List extends React.Component {
           <p>{this.state.warning}</p>
           <button onClick={()=>{this.addItem()}}>add item</button>
           <div>
-            <ToDoItem items={this.state.list} delete={this.delete}/>
+            <div className="todo">To Do:
+                <ToDoItem items={this.state.list} delete={this.delete}/>
+            </div>
+            <div className="deleted">
+                <DeletedItem deleted={this.state.deleted}/>
+            </div>
           </div>
         </div>
       );
