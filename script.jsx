fbbae60c88160eb4332e873ad1f1d5ac2cb03045
sharkyu3 <lowhuiyu@hotmail.com>
2019-09-01 19:41:30 +0800

    items list set up as a separate component

--- script.jsx
+++ script.jsx
@@ -1,3 +1,21 @@
+class ToDoItem extends React.Component {
+
+    render(){
+        console.log("inside item component: ", this.props.items);
+        const wordsList = this.props.items.map((word, index) => {
+        return (<div className="box">
+                <p>{word[0]}</p>
+                <p>{moment(word[1]).format("DD MMM YYYY, h:mm:ss a")}</p>
+                <button onClick={() => {this.deleteItem(index)}}>Delete this</button>
+            </div>);
+      });
+
+        return(<div>{wordsList}</div>)
+
+    }
+
+}
+
 class List extends React.Component {
   constructor(){
     super()
@@ -20,11 +38,11 @@ class List extends React.Component {
   }
 
   addItem(){
+    console.log("inside add item:", this.state);
     if(this.state.word.length >= 1 && this.state.word.length < 200){
         this.state.list.push([this.state.word, new Date()]);
         this.setState({word: "", warning: "", class:""});
     }else{
-        console.log("not within character count");
         this.setState({class: "warning"});
         this.setState({warning: "Input must be at least 1 character and not more than 200 characters"})
     }
@@ -35,26 +53,13 @@ class List extends React.Component {
   }
 
   render() {
-      // render the list with a map() here
-
-      console.log("rendering");
-      const wordsList = this.state.list.map((x, index) => {
-        return (
-            <div className="box">
-                <p>{x[0]}</p>
-                <p>{moment(x[1]).format("DD MMM YYYY, h:mm:ss a")}</p>
-                <button onClick={() => {this.deleteItem(index)}}>Delete this</button>
-            </div>
-            );
-      })
-
       return (
         <div className="list">
           <input onChange={()=>{this.changeHandler()}} value={this.state.word} className={this.state.class}/>
           <p>{this.state.warning}</p>
           <button onClick={()=>{this.addItem()}}>add item</button>
           <div>
-            {wordsList}
+            <ToDoItem items={this.state.list}/>
           </div>
         </div>
       );
